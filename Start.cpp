fdc053b01b20f6b4d4f825b5bea41470f19cf3d7
Nath <nathcuya@qiuhua.onmicrosoft.com>
2023-09-28 01:35:39 +0800

    Update Start.cpp

--- Start.cpp
+++ Start.cpp
@@ -2,12 +2,15 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     std::cout << "Hello, World!" << std::endl;
 
-    int a=5;
-    cout << a << endl;
+    int a = 5;
+    int *p;
+    p = &a;
 
+    cout << p << endl;
 
     return 0;
 }
