4cfcbce63bd68be3eb62845106b1b7739dd6fc70
Nath <nathcuya@qiuhua.onmicrosoft.com>
2023-10-16 23:33:06 +0800

    Update Exprise.cpp

--- Exprise.cpp
+++ Exprise.cpp
@@ -100,11 +100,3 @@ int main()
 //         for (int j = 0; j < 9; j++)
 //         {
 
-//             if (arr[j] < arr[j + 1])
-//             {
-//                 Allowance = arr[j + 1];
-//                 arr[j + 1]=arr[j] ;
-//                 arr[j]= Allowance;
-//             };
-//         };
-//     };
