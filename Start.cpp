1a4eb847974656fcb7b83464d6029b915bf990f2
Nath <nathcuya@qiuhua.onmicrosoft.com>
2023-10-18 23:06:39 +0800

    Update Start.cpp

---
+++ Start.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+void MenuShow()
+{
+    cout << "-----1.添加联系人-----" << endl;
+    cout << "-----2.显示联系人-----" << endl;
+    cout << "-----3.删除联系人-----" << endl;
+    cout << "-----4.更改联系人-----" << endl;
+    cout << "-----5.清空联系人-----" << endl;
+    cout << "-----6.查找联系人-----" << endl;
+    cout << "-----0.退出本系统-----" << endl;
+
+}
+
+int main()
+{
+
+
+    MenuShow();
+    return 0;
+}
